caff9bfc24a5b747d6dcf5dd0abbd6b85d726684
evan <chanjungskim@gmail.com>
2023-06-16 01:03:42 +0900

    Fix: AGP downgrade to 8.0.1

--- compose/src/main/java/com/github/chanjungkim/compose/LazyScrollColumn.kt
+++ compose/src/main/java/com/github/chanjungkim/compose/LazyScrollColumn.kt
@@ -1,6 +1,5 @@
 package com.github.chanjungkim.compose
 
-import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.fadeIn
@@ -27,11 +26,9 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
